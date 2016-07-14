5c73dae30f65326feaede0fdb6b5ede1be8f4542
Blake Gilmore <blakegilmore@carfax.com>
2016-07-14 16:47:11 -0500

    Flesh out soda class more

--- src/main/java/hello/Soda.java
+++ src/main/java/hello/Soda.java
@@ -1,16 +1,20 @@
 package hello;
 
+import java.lang.reflect.Array;
+
 /**
  * Created by blakegilmore on 7/13/16.
  */
 public class Soda {
     String type;
     String brand;
-    int currentPosition;
-    float price;
+    Array currentPosition;
+    double price;
 
-    Soda(String type,String brand){
+    Soda(String type,String brand, Array currentPosition){
         this.type = type;
         this.brand = brand;
+        this.currentPosition = currentPosition;
+        this.price = 1.50;
     }
 }
