61199968ebca792b23d20b4017811b42d4bcf183
Blake Gilmore <blakegilmore@carfax.com>
2016-07-13 20:28:29 -0500

    Create inventory and compare to inventory

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,11 +1,13 @@
 package hello;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * Created by blakegilmore on 7/13/16.
  */
 public class Application {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useDelimiter("\n");
         int x = 0;
@@ -14,9 +16,19 @@ public class Application {
         while (x < 2) {
             System.out.println("Please choose drink A or B");
             String input = scanner.next();
-            System.out.println(input);
+
+            String[] inventory = new String[2];
+            inventory[0] = "A";
+            inventory[1] = "B";
+
+            if (Arrays.asList(inventory).contains(input)) {
+                System.out.println("Here is your drink");
+                System.out.println(input);
+            }
+
             x += 1;
 
         }
+
     }
 }
--- src/main/java/hello/InventoryService.java
+++ src/main/java/hello/InventoryService.java
@@ -3,6 +3,9 @@ package hello;
 /**
  * Created by blakegilmore on 7/13/16.
  */
-public class InventoryService {
-
-}
+//public class InventoryService {
+//    String[] inventory = new String[2];
+//    inventory[0] = "A";
+//    inventory[1] = "B";
+//
+//}
