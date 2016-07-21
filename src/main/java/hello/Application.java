12932bf60afa4c986672c3097aa5213fafb66554
Blake Gilmore <blakegilmore@carfax.com>
2016-07-21 11:44:14 -0500

    Add spring functionality

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,12 +1,19 @@
 package hello;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by blakegilmore on 7/13/16.
  */
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) throws InterruptedException {
         Selection mySelection = new Selection();
         mySelection.selectSoda();
+        SpringApplication.run(Application.class, args);
     }
 }
+
+
