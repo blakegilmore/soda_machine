f92cc1b0dabb99fcb075f8a49eac2066a6f80f0e
Blake Gilmore <blakegilmore@carfax.com>
2016-07-22 08:33:29 -0500

    Add controllers for inventory

--- src/main/java/hello/InventoryController.java
+++ src/main/java/hello/InventoryController.java
@@ -1,15 +1,39 @@
 package hello;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class InventoryController {
 
+    Inventory inventory;
+
     @RequestMapping("/inventory")
-    public Inventory inventory() {
-        return new Inventory();
+    public Greeting inventory() {
+        return new Greeting(0,"hello");
+    }
+
+    @RequestMapping(value = "/updateSoda", method = RequestMethod.PATCH)
+    public @ResponseBody Soda updateSoda(@RequestParam(value="newBrand", defaultValue="Coke") String newBrand){
+        return new Soda("Pepsi");
+    }
+
+
+
+    private Soda convertBrand(String newBrand, Soda soda){
+         soda.brand = newBrand;
+        return soda;
     }
 }
+
+//[soda1, soda2, soda3]
+//
+//        {
+//            date: "7-21-2016",
+//            numOfCokes: 4,
+//            numOfPepsis: 6,
+//            inventory: [soda1, soda2, soda3]
+//        }
--- src/main/java/hello/Soda.java
+++ src/main/java/hello/Soda.java
@@ -6,6 +6,33 @@ import java.util.concurrent.atomic.AtomicInteger;
  * Created by blakegilmore on 7/13/16.
  */
 public class Soda {
+    public String getBrand() {
+        return brand;
+    }
+
+    public Soda setBrand(String brand) {
+        this.brand = brand;
+        return this;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Soda setPrice(double price) {
+        this.price = price;
+        return this;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Soda setId(int id) {
+        this.id = id;
+        return this;
+    }
+
     String brand;
     double price;
     private static final AtomicInteger count = new AtomicInteger(0);
