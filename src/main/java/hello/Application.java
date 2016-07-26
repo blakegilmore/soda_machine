c96eea0441ab051288696f4f645a430e811d2c71
Blake Gilmore <blakegilmore@carfax.com>
2016-07-25 22:49:52 -0500

    Add SQL database

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.sql.*;
 
 
 import java.util.Arrays;
@@ -14,11 +15,42 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application {
+
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) throws InterruptedException {
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            // handle the error
+            System.out.println("There was an error with the driver");
+        }
+
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" + "user=root&password=password");
+
+            // Do something with the Connection
+            stmt = conn.createStatement();
+            stmt.executeQuery("DROP TABLE sodas IF EXISTS");
+            stmt.executeQuery("CREATE TABLE sodas(" +
+                    "id SERIAL, brand VARCHAR(255), price INT)");
+            
+            /// add a soda to the table with a given brand name
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+
         Selection mySelection = new Selection();
         mySelection.selectSoda();
         SpringApplication.run(Application.class, args);
@@ -27,32 +59,7 @@ public class Application implements CommandLineRunner {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    @Override
-    public void run(String... strings) throws Exception {
-
-        log.info("Creating tables");
-
-        jdbcTemplate.execute("DROP TABLE customers IF EXISTS");
-        jdbcTemplate.execute("CREATE TABLE customers(" +
-                "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
-
-        // Split up the array of whole names into an array of first/last names
-        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
-                .map(name -> name.split(" "))
-                .collect(Collectors.toList());
-
-        // Use a Java 8 stream to print out each tuple of the list
-        splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
-
-        // Uses JdbcTemplate's batchUpdate operation to bulk load data
-        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
-
-        log.info("Querying for customer records where first_name = 'Josh':");
-        jdbcTemplate.query(
-                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[] { "Josh" },
-                (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
-        ).forEach(customer -> log.info(customer.toString()));
-    }
 }
+
 
 
